27b6e72bb8944a9552505e5a7577ed16dce03eb9
Todd <ihatespam@gmail.com>
2024-02-29 05:35:56 -0600

    Update index2.js

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React from "react"
+import { useState, useEffect } from "react";
 import Layout from "../components/siteLayout";
 import Seo from "../components/seo";
 import VideoPlayer from "../components/VideoPlayer";
 import { Helmet } from "react-helmet";
-
+import HomePosts from "../components/HomePosts";
+import useSiteMetadata from "../hooks/SiteMetadata";
 const HomePage1 = ({ location }) => {
+
+
+  const { featureOptions  } = useSiteMetadata();
+
+
+  const { showDefault } = featureOptions
+  const isLocalStorageAvailable = typeof window !== "undefined" && window.localStorage;
+
+  const storedValue = isLocalStorageAvailable ? localStorage.getItem("isSliderVisible") : null;
+  const initialSliderVisible = storedValue ? JSON.parse(storedValue) : showDefault;
+
+  const [isSliderVisible, setIsSliderVisible] = useState(initialSliderVisible);
+
+
+
+  useEffect(() => {
+    if (isLocalStorageAvailable) {
+      // Update isSliderVisible when it changes in localStorage
+      const handleStorageChange = () => {
+        const storedValue = localStorage.getItem("isSliderVisible");
+        try {
+          setIsSliderVisible(JSON.parse(storedValue) ?? true);
+        } catch (error) {
+          setIsSliderVisible(true);
+        }
+      };
+
+      // Add event listener for storage change
+      window.addEventListener("storage", handleStorageChange);
+
+      // Cleanup function to remove event listener
+      return () => {
+        window.removeEventListener("storage", handleStorageChange);
+      };
+    }
+  }, [isLocalStorageAvailable]);
+
+
   return (
     <Layout>
       <Helmet>
@@ -13,6 +53,7 @@ const HomePage1 = ({ location }) => {
       <SeoWrapper location={location} />
       <div className='player-wrapper'>
         <VideoPlayer location={location} />
+        <HomePosts isSliderVisible={isSliderVisible} className="scroll-area" id="posttop" name="posttop" style={{minHeight:'100dvh', width:'100vw'}} />
       </div>
     </Layout>
   );
