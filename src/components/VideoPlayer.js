0fa90cffd846a2ecc18aca07fb54328b9566e12a
Todd <ihatespam@gmail.com>
2024-03-10 04:27:43 -0500

    Update VideoPlayer.js

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -417,7 +417,7 @@ const handleAutoplayChange = (event) => {
         // transform: hideEditor ? 'translateY(-100%)' : 'none',
         transition: 'transform 0.5s ease-in-out',
         background: 'var(--theme-ui-colors-headerColor)',
-        color:'--theme-ui-colors-headerColorText'
+        // color:'--theme-ui-colors-headerColorText'
         
         // height: hideEditor ? '0' : 'auto'
 
